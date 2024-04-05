3e4da879b14b53084759015282dd43b51131a928
Tales <81427667+talesviana@users.noreply.github.com>
2024-04-05 20:27:22 -0300

    Add files via upload

---
+++ src/main/java/br/com/bibliotecaspring/bibliotecaSpring/controller/Estante.java
@@ -0,0 +1,38 @@
+package controller;
+
+public class Estante {
+	private int qtdLivro;
+	private String nome;
+	private String descricao;
+	
+	public void adicionarLivro() {
+		
+	}
+	
+	public void removerLivro() {
+		
+	}
+	
+	public int getQtdLivro() {
+		return qtdLivro;
+	}
+	public void setQtdLivro(int qtdLivro) {
+		this.qtdLivro = qtdLivro;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+	
+	
+}
